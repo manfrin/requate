09341c3b829d730e90ef323f38beece845f76ee4
Mike Manfrin <mikem@gopago.com>
2013-05-28 21:31:43 -0700

    Initial rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reqate::Application.config.secret_token = '78116bebc57265d882d409caf6494d4652671e716ffca0f7f93e9ed72b173db837098fac83736baf9886e37ef88ebd2f6527d716b23960a105224b5759039386'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Reqate::Application.config.session_store :cookie_store, key: '_reqate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Reqate::Application.config.session_store :active_record_store
