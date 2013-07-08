b7ec6ab7b8ab2fc79525d2d4937106a931232aba
Mike Manfrin <mikem@gopago.com>
2013-07-08 16:12:54 -0700

    More work on equalization.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -7,11 +7,69 @@ class Bill < ActiveRecord::Base
   after_create :initial_bill_payments
 
   def initial_bill_payments
-  	roommates = household.roommates
-  	split_amount = cent_value / roommates.count
+  	roommates    = household.roommates
+  	split_amount = cent_value / roommates.count # To be expanded
+    roommate     = Roommate.find(roommate_id)
 
-  	roommates.each do |r|
-  		Payment.create(from: roommate_id, to: r.id, cent_value: split_amount, initial_bill_split: true, related_bill_id: self.id) unless r.id == roommate_id
+  	roommates.each do |roommate_unpaid|
+
+      next if roommate_unpaid.id == roommate_id
+
+      roommate_split = split_amount
+
+      owes_to = roommate.owes_to_roommate(roommate_unpaid)
+
+      all_params = {
+        from: roommate_id, 
+        to: roommate_unpaid.id, 
+        related_bill_id: self.id
+      }
+
+      payment_params = {
+        cent_value: roommate_split,      
+        initial_bill_split: true ,
+        repayment_needed: true
+      }
+
+      if owes_to > 0  # In the case bill-payer owes more than $0
+
+        payments = Payment.where(to: roommate_id, from: roommate_unpaid.id, repayment_needed: true)
+
+        payments.each do |payment|
+          if payment.cent_value < roommate_split
+
+            split_params = { cent_value: payment.cent_value,
+                             initial_bill_split: false,
+                             repayment_needed: false }
+
+            Payment.create(split_params.merge!(all_params))
+
+            payment.update_attributes(repayment_needed: false)
+
+            roommate_split -= payment.cent_value
+          else
+            last_split_params = { cent_value: roommate_split,
+                                  initial_bill_split: true,
+                                  repayment_needed: true }
+
+            Payment.create(last_split_params.merge!(all_params))
+          end
+        end
+
+        # Remainder 
+        if roommate_split > 0
+          last_split_params = { cent_value: roommate_split,
+                                initial_bill_split: true,
+                                repayment_needed: true }
+
+          Payment.create(last_split_params.merge!(all_params))
+        end
+
+      else
+  		  Payment.create(payment_params.merge!(all_params)) 
+      end
   	end
+
   end
+
 end	
